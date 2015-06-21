e56f538185339f0b1cd316b11183d4ad951055ea
bjarte <b.sjursen@gmail.com>
2015-06-21 17:36:42 +0200

    added ExerciseTableViewController

---
+++ MAX/ExerciseTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ExerciseTableViewController.swift
+//  MAX
+//
+//  Created by Bjarte Sjursen on 21.06.15.
+//  Copyright (c) 2015 Bjarte Sjursen. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseTableViewController: NSObject {
+   
+}
