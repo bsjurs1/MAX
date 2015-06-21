90b209c06c5783212c3c55943d50fa0afb33ab00
Bjarte Sjursen <b.sjursen@gmail.com>
2015-06-21 17:42:37 +0200

    Added ExerciseTableViewCell

---
+++ MAX/ExerciseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExerciseTableViewCell.swift
+//  MAX
+//
+//  Created by Bjarte Sjursen on 21.06.15.
+//  Copyright (c) 2015 Bjarte Sjursen. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
