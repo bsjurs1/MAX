48eb8df2627b0734b53bf14b139fb461eeaab17d
Bjarte Sjursen <b.sjursen@gmail.com>
2015-06-21 17:56:09 +0200

    Added alphabetic index

--- MAX/ExerciseTableViewController.swift
+++ MAX/ExerciseTableViewController.swift
@@ -22,18 +22,25 @@ class ExerciseTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    /*- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
+    return[NSArray arrayWithObjects:@"a", @"e", @"i", @"m", @"p", nil];
+    }*/
+    
+    override func sectionIndexTitlesForTableView(tableView: UITableView) -> [AnyObject]! {
+        
+        return ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+        
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
+        return 26
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
+        
         return 0
     }
 
