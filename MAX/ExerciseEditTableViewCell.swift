074b65d3021849e10c70b5009f14ef1c9403d49a
Bjarte Sjursen <b.sjursen@gmail.com>
2015-06-24 10:38:06 +0200

    Added the ExerciseEditTableViewCell, and connected the outlets

---
+++ MAX/ExerciseEditTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ExerciseEditTableViewCell.swift
+//  MAX
+//
+//  Created by Bjarte Sjursen on 24.06.15.
+//  Copyright (c) 2015 Bjarte Sjursen. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseEditTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var exerciseDetailsButton: UIButton!
+    @IBOutlet weak var addSetButton: UIButton!
+    @IBOutlet weak var editExerciseButton: UIButton!
+    @IBOutlet weak var exerciseNameLabel: UILabel!
+    @IBOutlet weak var exerciseImageView: UIImageView!
+    @IBOutlet weak var setsTableView: UITableView!
+    
+    
+
+}
