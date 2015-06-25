f826e26d8c3c1115e6d4cad01279cdf995bb13e1
Bjarte Sjursen <b.sjursen@gmail.com>
2015-06-25 15:28:02 +0200

    Added a UIImageView to the ExerciseRoutineCollectionView, and added a controller for it

---
+++ MAX/ExerciseRoutinesCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ExerciseRoutinesCollectionViewController.swift
+//  MAX
+//
+//  Created by Bjarte Sjursen on 25.06.15.
+//  Copyright (c) 2015 Bjarte Sjursen. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseRoutinesCollectionViewController: UICollectionViewController {
+
+}
