dae78ce773c7e54f760b298bd468557bc54738fc
Bjarte Sjursen <b.sjursen@gmail.com>
2015-06-26 20:26:31 +0200

    Made createRoutineViewController in both storyboard and as a swift class

---
+++ MAX/CreateRoutineViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CreateRoutineViewController.swift
+//  MAX
+//
+//  Created by Bjarte Sjursen on 26.06.15.
+//  Copyright (c) 2015 Bjarte Sjursen. All rights reserved.
+//
+
+import UIKit
+
+class CreateRoutineViewController: UIViewController {
+    
+    
+
+}
